b187f9cc3fd8ebaa8abeee0198d7c79c80fed89c
Rudy Affandi <rudy@adnetinc.com>
2016-07-26 17:21:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Auth0 OAuth2 Provider for Laravel Socialite
+
+[![Scrutinizer Code Quality](https://img.shields.io/scrutinizer/g/SocialiteProviders/Auth0.svg?style=flat-square)](https://scrutinizer-ci.com/g/SocialiteProviders/Auth0/?branch=master)
+[![Latest Stable Version](https://img.shields.io/packagist/v/socialiteproviders/auth0.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/auth0)
+[![Total Downloads](https://img.shields.io/packagist/dt/socialiteproviders/auth0.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/auth0)
+[![Latest Unstable Version](https://img.shields.io/packagist/vpre/socialiteproviders/auth0.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/auth0)
+[![License](https://img.shields.io/packagist/l/socialiteproviders/auth0.svg?style=flat-square)](https://packagist.org/packages/socialiteproviders/auth0)
+
+## Documentation
+
+Full documentation for using this provider can be found at [Auth0 Documentation](http://socialiteproviders.github.io/providers/auth0/)
---
+++ src/Provider.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace SocialiteProviders\Auth0;
+
+use Laravel\Socialite\Two\ProviderInterface;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'AUTH0';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [''];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase($this->getInstanceUri() . 'authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return $this->getInstanceUri() . 'oauth/access_token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get($this->getInstanceUri() . 'userinfo', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'nickname' => $user['username'],
+            'name'     => $user['name'],
+            'email'    => $user['email'],
+            'avatar'   => $user['avatar'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code'
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getInstanceUri()
+    {
+        return $this->getConfig('instance_uri', 'https://auth0.com/');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function additionalConfigKeys()
+    {
+        return ['instance_uri'];
+    }
+
+}
